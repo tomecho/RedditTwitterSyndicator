971ff80a40c4b6fa9d34fc38a01acfadf0028e28
Tom Peck <tapeck@umass.edu>
2019-01-10 22:19:45 -0500

    Write function to get top posts with very ugly struct to deserialize data

--- RedditPull.cs
+++ RedditPull.cs
@@ -2,8 +2,13 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using RestSharp;
+using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace RedditTwitterSyndicator
 {
@@ -15,6 +20,59 @@ namespace RedditTwitterSyndicator
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             string accessToken = GetAccessToken(log);
+            List<RedditPost> redditPosts = GetTopPosts(log, accessToken);
+        }
+
+        private static void WriteToTable(ILogger log, dynamic posts)
+        {
+            var storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+            var tableClient = storageAccount.CreateCloudTableClient();
+            
+            // todo write to table
+        }
+
+        struct RedditPost
+        {
+            public string Title { get; set; }
+            public string Url { get; set; }
+        }
+
+        struct SubRedditPostQueryResponse
+        {
+            public struct QueryResponseData
+            {
+                public struct QueryResponseChildren
+                {
+                    public struct QueryResponsePostData
+                    {
+                        public string Title { get; set; }
+                        public string Url { get; set; }
+                    }
+                    public QueryResponsePostData Data { get; set; }
+                }
+                public List<QueryResponseChildren> Children { get; set; }
+            }
+            public QueryResponseData Data { get; set; }
+        }
+
+        private static List<RedditPost> GetTopPosts(ILogger log, string accessToken)
+        {
+            RestClient client = new RestClient("https://oauth.reddit.com");
+            RestRequest request = new RestRequest("/r/me_irl/top", Method.GET);
+            request.AddHeader("User-Agent", Environment.GetEnvironmentVariable("USER_AGENT"));
+            request.AddHeader("x-li-format", "json");
+            request.AddHeader("Authorization", $"bearer {accessToken}");
+            request.AddQueryParameter("t", "day");
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                log.LogCritical("Failed to read top reddit posts");
+            }
+
+            var responseContent = JsonConvert.DeserializeObject<SubRedditPostQueryResponse>(response.Content.ToString());
+            return responseContent.Data.Children.Select(post => new RedditPost() { Title = post.Data.Title, Url = post.Data.Url }).ToList();
         }
 
         private static string GetAccessToken(ILogger log)
